79c4fd8963b6a3a590d38ae00cd87b2fb80f537c
vaibhav <vaibhav.vkj@gmail.com>
2015-09-18 18:22:38 +0530

    delete all

--- HWM-Material/js/controller/customerController.js
+++
@@ -1,6 +0,0 @@
-app.controller('customerCtrl',function($scope,$http){
-    $http.post('https://localhost:8443/ofbizdemo/control/findAllPerson')
-        .success(function(response){
-            $scope.dataReceived=response.mylist;
-          });
-});
