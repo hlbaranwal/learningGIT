c93e6a34c2476e35f473661479fd5fb0310c89f6
vaibhav <vaibhav.vkj@gmail.com>
2015-09-24 13:50:17 +0530

    bebo

--- HWM-Material/js/controller/createCustomerController.js
+++ HWM-Material/js/controller/createCustomerController.js
@@ -1,10 +1,10 @@
 app.controller('createCustomerCtrl',function($scope,$http){
-	$scope.createPerson=function(){
-        $http.post('https://localhost:8443/partymgr/control/createPerson',{firstName:$scope.firstName,lastName:$scope.lastName,emailId:$scope.emailId}).success(function(response){
+	$scope.createCustomer=function(){
+        $http.post('https://localhost:8443/ofbizdemo/control/createCustomer',{firstName:$scope.firstName,lastName:$scope.lastName,emailAddress:$scope.emailId}).success(function(response){
         $scope.dataReceived=response;
         });
     }
-    $scope.myf=function(){
+    /*$scope.myf=function(){
         alert("Customer is created!");
-    }
+    }*/
 });
