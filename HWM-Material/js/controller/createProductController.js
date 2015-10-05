efe4149f8efab687852561f9fd2dd545e2fe2fc2
vaibhav <vaibhav.vkj@gmail.com>
2015-10-05 18:00:32 +0530

    remove conflicts

--- HWM-Material/js/controller/createProductController.js
+++ HWM-Material/js/controller/createProductController.js
@@ -1,11 +1,7 @@
 app.controller('createProductCtrl',function($scope,$http){
 	$scope.createProduct=function(){
-<<<<<<< HEAD
       	$http.post('https://localhost:8443/ofbizdemo/control/createProductService',{productId:$scope.productId,productName:$scope.productName,price:$scope.price,productCode:$scope.productCode,weight:$scope.weight}).success(function(response){
       	$scope.dataReceived=response;
         });  
-=======
-        
->>>>>>> 9265eafefa8b90a4c44df3454eba0a9634d24792
     }
     });
