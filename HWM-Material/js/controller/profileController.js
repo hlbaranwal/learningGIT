343db5c973166947e1cd1e30d3c3d3c3a1dcf367
vaibhav <vaibhav.vkj@gmail.com>
2015-10-05 12:01:14 +0530

    Add options in profile.html

--- HWM-Material/js/controller/profileController.js
+++ HWM-Material/js/controller/profileController.js
@@ -29,10 +29,100 @@ app.controller('profileCtrl',function($scope,$http,$mdDialog){
     });
   };
     
+    $scope.showAddMailAddress = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'partials/customerService/addMailAddress.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    
     $scope.showAddUserLogin = function(ev) {
     $mdDialog.show({
       controller: DialogController,
       templateUrl: 'partials/customerService/addUserLogin.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    
+    $scope.showAddQualification = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'partials/customerService/addQualification.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    
+    $scope.showAddShippingAddress = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'partials/customerService/addShippingAddress.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    
+    $scope.showAddCreditCard = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'partials/customerService/addCreditCard.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    
+    $scope.showAddSalesChannel = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'partials/customerService/addSalesChannel.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    
+    $scope.showAddClassificationGroup = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'partials/customerService/addClassificationGroup.html',
       parent: angular.element(document.body),
       targetEvent: ev,
       clickOutsideToClose:true
